2b21c784a60b820b68c974ab36bfcb98f3b03585
kimNarr <ignitionhero@gmail.com>
2024-12-10 10:43:00 +0900

    파일 및 코드 구조 정리

--- js/common.js
+++ js/common.js
@@ -66,163 +66,30 @@ function mNavTrigger () {
 mNavTrigger();
 
 
-// 메인비주얼 스크롤 이벤트
-gsap.registerPlugin(ScrollTrigger);
-
-let mainText = document.querySelector(".mainView .inner .textBox");
-
-gsap.to(mainText, {
-    scale: 0.3, 
-    autoAlpha: 0, 
-    ease: "power2",
-    scrollTrigger: {
-        trigger: mainText,
-        scrub: true,
-        start: "top top",
-        end: "bottom -=100%",
-        // markers : true,
-    },
-});
-
-
-// 프로젝트 섹션 fade effect
-let project = document.querySelector(".project");
-
-gsap.to(project,{
-    scrollTrigger: {
-        trigger: mainText,
-        scrub: true,
-        start: "center 30%",
-        end: "center 30%",
-        // markers: true,
-        onEnter: function() {
-            project.classList.add("on")
-        },
-        onLeaveBack: function() {
-            project.classList.remove("on")
-        },
-    },
-});
-
-// 파트너 섹션 fade effect
-let partner = document.querySelector(".partner");
-
-gsap.to(partner,{
-    scrollTrigger: {
-        trigger: project,
-        scrub: true,
-        start: "top top",
-        end: "top top",
-        // markers: true,
-        onEnter: function() {
-            partner.classList.add("on")
-        },
-        onLeaveBack: function() {
-            partner.classList.remove("on")
-        },
-    },
-});
-
-// alumni 섹션 fade effect
-let alumni = document.querySelector(".alumni");
-
-gsap.to(alumni,{
-    scrollTrigger: {
-        trigger: project,
-        scrub: true,
-        start: "center top",
-        end: "center top",
-        // markers: true,
-        onEnter: function() {
-            alumni.classList.add("on")
-        },
-        onLeaveBack: function() {
-            alumni.classList.remove("on")
-        },
-    },
-});
-
-// performance 섹션 fade effect
-let performance = document.querySelector(".performance");
-
-gsap.to(performance,{
-    scrollTrigger: {
-        trigger: alumni,
-        scrub: true,
-        start: "top top",
-        end: "top top",
-        // markers: true,
-        onEnter: function() {
-            performance.classList.add("on")
-        },
-        onLeaveBack: function() {
-            performance.classList.remove("on")
-        },
-    },
-});
-
-// performance 섹션 number count up
-let countUpTriggers = document.querySelectorAll(".numberBox ul li");
-
-countUpTriggers.forEach(trigger => {
-    let countUpTarget = trigger.querySelector(".num");
-
-    let countElement = {
-        from: countUpTarget.dataset.from,
-        to: countUpTarget.dataset.to
-    };
-
-    let numberElement = {
-        count: countElement.from
-    };
-
-    gsap.to(numberElement, {
-        count: countElement.to,
-        duration: 2,
-        stagger: 1,
-        scrollTrigger: {
-            trigger: alumni,
-            start: "top top",
-            // markers: true,
-            toggleActions: "play none none reset",
-        },
-        onUpdate: ()=>{
-            if(countUpTarget.classList.contains("n01")) {
-                countUpTarget.textContent = `${Math.floor(numberElement.count)}+`;
-            } else if(countUpTarget.classList.contains("n04")) {
-                countUpTarget.textContent = Math.floor(numberElement.count).toLocaleString();
-            } else {
-                countUpTarget.textContent = Math.floor(numberElement.count);
-            }      
-        }
-    });
-});
-
-// newsletter 섹션 fade effect
-let newsletter = document.querySelector(".newsletter");
-let subscribeBtn = newsletter.querySelector(".inner .subscribe button")
-
-gsap.to(newsletter,{
-    scrollTrigger: {
-        trigger: performance,
-        scrub: true,
-        start: "top top",
-        end: "top top",
-        // markers: true,
-        onEnter: function() {
-            newsletter.classList.add("on");
-            subscribeBtn.classList.add("on");
-        },
-        onLeaveBack: function() {
-            newsletter.classList.remove("on")
-            subscribeBtn.classList.remove("on");
-        },
-    },
-});
-
 // footer family site
 function familySite() {
     let familySite = document.querySelector("#family");
 
     window.open(familySite.value);
 };
+
+// top btn
+let topBtn = document.querySelector(".topBtn");
+let bodyArea = document.querySelector("body");
+
+gsap.to(topBtn,{
+    scrollTrigger: {
+        trigger: bodyArea,
+        start: "+=150 top",
+        onEnter: function() {
+            topBtn.classList.add("on");
+        },
+        onLeaveBack: function() {
+            topBtn.classList.remove("on");
+        }
+    }
+});
+
+topBtn.addEventListener("click", ()=> {
+    window.scrollTo({top:0, behavior:"smooth"})
+});
---
+++ js/sub.js
@@ -0,0 +1,10 @@
+// subpage title animation
+let subtTitle = document.querySelector(".sub .inner .title h2");
+let subText = document.querySelector(".sub .inner .title p");
+let subSimbol = document.querySelector(".sub .inner .simbol");
+
+function subTitleAnimation() {
+    subTitle.classList.add("on");
+    subText.classList.add("on");
+    subText.classList.add("on");
+}

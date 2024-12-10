8c62cd8967e85f842f6596e3632912d816aa514c
kimNarr <ignitionhero@gmail.com>
2024-12-10 11:08:45 +0900

    sub title animation add

--- js/sub.js
+++ js/sub.js
@@ -1,10 +1,11 @@
 // subpage title animation
-let subtTitle = document.querySelector(".sub .inner .title h2");
+let subTitle = document.querySelector(".sub .inner .title h2");
 let subText = document.querySelector(".sub .inner .title p");
 let subSimbol = document.querySelector(".sub .inner .simbol");
 
 function subTitleAnimation() {
     subTitle.classList.add("on");
     subText.classList.add("on");
-    subText.classList.add("on");
+    subSimbol.classList.add("on");
 }
+subTitleAnimation()

c561087b71a553ebcd2ab6f9268b1029259d670f
kimNarr <ignitionhero@gmail.com>
2024-12-12 10:55:09 +0900

    js 파일 정리 및 sub page 추가 작업

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ gsap.to(mainText, {
     },
 });
 
-
 // 프로젝트 섹션 fade effect
 let project = document.querySelector(".project");
 
@@ -33,6 +32,23 @@ gsap.to(project,{
         onLeaveBack: function() {
             project.classList.remove("on")
         },
+    },
+});
+
+// 프로젝트 섹션 슬라이드
+let projectSwiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    effect: "fade",
+    loop: true,
+    centeredSlides: true,
+    pagination: {
+        el: '.project .swiper-pagination',
+        type : 'fraction',
+    },
+    navigation: {
+        nextEl: '.project .button-next',
+        prevEl: '.project .button-prev',
     },
 });
 
@@ -54,6 +70,340 @@ gsap.to(partner,{
         },
     },
 });
+
+// alumni listup & marquee
+let mqItem01 = [
+    {   
+        id: 1,
+        name : "베링랩",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#AI Startup Accelerator 1기"
+    },
+    {   
+        id: 2,
+        name : "에이슬립",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#AI Startup Accelerator 1기"
+    },
+    {   
+        id: 3,
+        name : "파우스트",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#AI Startup Accelerator 1기"
+    },
+    {   
+        id: 4,
+        name : "폴라리스쓰리디",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#AI Startup Accelerator 1기"
+    },
+    {   
+        id: 5,
+        name : "오아시스비즈니스",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#AI Startup Accelerator 1기"
+    },
+    {   
+        id: 6,
+        name : "업스테이지",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#AI Startup Accelerator 1기"
+    },
+    {   
+        id: 7,
+        name : "스팍랩",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "-"
+    },
+    {   
+        id: 8,
+        name : "드랩",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "-"
+    },
+    {   
+        id: 9,
+        name : "허드슨에이아이",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "-"
+    },
+    {   
+        id: 10,
+        name : "빌리뷰",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : " #리뷰분석 #커머스AI"
+    },
+    {   
+        id: 11,
+        name : "누비랩",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#ESG #음식물쓰레기 #지속가능성 #단체급식소"
+    },
+    {   
+        id: 12,
+        name : "식스티헤르츠 주식회사",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#가상발전소 #재생에너지 #소셜벤처"
+    },
+    {   
+        id: 13,
+        name : "주식회사 모두의셔틀",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#멤버십셔틀 #버스모빌리티 #다이나믹셔틀 #수요응답형버스 #온디맨드버스"
+    },
+    {   
+        id: 14,
+        name : "(주)엔젤스윙",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#Innovative Technology for All, #드론, #스마트건설"
+    },
+];
+
+let mqItem02 = [
+    {   
+        id: 15,
+        name : "㈜메디아이플러스",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#임상시험 #디지털헬스케어"
+    },
+    {   
+        id: 16,
+        name : "서울다이나믹스",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#True Innovation 3기"
+    },
+    {   
+        id: 17,
+        name : "딥네츄럴",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#True Innovation 3기"
+    },
+    {   
+        id: 18,
+        name : "아티피셜소사이어티",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#AI Startup Accelerator 1기"
+    },
+    {   
+        id: 19,
+        name : "린솔",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#ESG Korea 2024"
+    },
+    {   
+        id: 20,
+        name : "비젼인",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#ESG Korea 2024"
+    },
+    {   
+        id: 21,
+        name : "네이션에이",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#AI Startup Accelerator 1기"
+    },
+    {   
+        id: 22,
+        name : "에이트테크",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#AI로봇 #ESG Korea 2023"
+    },
+    {   
+        id: 23,
+        name : "소리를보는통로",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "-"
+    },
+    {   
+        id: 24,
+        name : "스퀴즈비츠",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "-"
+    },
+    {   
+        id: 25,
+        name : "오내피플",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#개인정보보호 #보안 #개인정보 #자동화"
+    },
+    {   
+        id: 26,
+        name : "주식회사 디스에이블드",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#발달장애예술가 #발달장애 #ESGSERVICE #예술"
+    },
+    {   
+        id: 27,
+        name : "애프터레인 주식회사",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#공기정화벤치 #미세먼지저감벤치 #공기질측정디스플레이 #그린뉴딜"
+    },
+    {   
+        id: 28,
+        name : "주식회사 웰로",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#정책 #추천 #자동신청 #타겟팅 #wello"
+    },
+    {   
+        id: 29,
+        name : "이노버스",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#친환경 #자원순환 #AI #IoT #제조 #ESG #스마트시티 #Sustainable"
+    },
+    {   
+        id: 30,
+        name : "코딧(CODIT)",
+        text : "AI음성인식을 활용한 실시간 자막지원서비스",
+        hashtag : "#대관 #정책매니저 #법 #규제 #정책 #모니터링 #B2G"
+    },
+]
+
+let marquee01 = document.querySelector(".marquee > ul:first-child");
+let marquee02 = document.querySelector(".marquee > ul:last-child");
+
+function firstMarquee(itemList) {
+    let items = itemList.map((item)=> {
+        return `
+        <li>
+            <a href="javascript:void(0);">
+                <div class="default">
+                    <span class="text">${item.name}</span>
+                    <span class="img"><img src="./img/alumni/a${item.id}.png" alt="${item.name}"></span>                </div>
+                <div class="hover">
+                    <h3 class="name">${item.name}</h3>
+                    <p class="text">${item.text}</p>
+                    <p class="hashtag">${item.hashtag}</p>
+                </div>
+            </a>
+        </li>
+        `
+    })
+    items = items.join("");
+    marquee01.innerHTML = items;
+}
+firstMarquee(mqItem01);
+
+function secondMarquee(itemList) {
+    let items = itemList.map((item)=> {
+        return `
+        <li>
+            <a href="javascript:void(0);">
+                <div class="default">
+                    <span class="text">${item.name}</span>
+                    <span class="img"><img src="./img/alumni/a${item.id}.png" alt="${item.name}"></span>                </div>
+                <div class="hover">
+                    <h3 class="name">${item.name}</h3>
+                    <p class="text">${item.text}</p>
+                    <p class="hashtag">${item.hashtag}</p>
+                </div>
+            </a>
+        </li>
+        `
+    })
+    items = items.join("");
+    marquee02.innerHTML = items;
+}
+secondMarquee(mqItem02);
+
+
+let marqueeBox = document.querySelectorAll(".marquee ul");
+let marqueeList = document.querySelectorAll(".marquee ul > li");
+
+function marqueeHover() {
+    marqueeBox.forEach(ul => {
+        if(window.innerWidth > 767) {
+            ul.addEventListener("mouseenter",()=>{
+                ul.style.position = "relative";
+                ul.style.zIndex = "10";
+            });
+            ul.addEventListener("mouseleave",()=>{
+                ul.style.position = "";
+                ul.style.zIndex = "";
+            });
+        } else {
+            ul.addEventListener("mouseenter",()=>{
+                ul.style.position = "";
+                ul.style.zIndex = "";
+            });
+            ul.addEventListener("mouseleave",()=>{
+                ul.style.position = "";
+                ul.style.zIndex = "";
+            });
+        }
+    });
+
+    marqueeList.forEach(li => {
+        let hoverBox = li.querySelector("a > .hover")
+        hoverBox.style.translate = "none";
+        hoverBox.style.rotate = "none";
+        hoverBox.style.scale = "none";
+        li.addEventListener("mousemove", (e)=>{
+            if(window.innerWidth > 767){
+                gsap.set(hoverBox,{
+                    x : e.offsetX,
+                    y : e.offsetY
+                });
+            } else {
+                gsap.set(hoverBox,{
+                    x : "",
+                    y : "",
+                });
+            }
+        });
+    });
+};
+marqueeHover();
+
+let body = document.querySelector("body");
+
+function mobilePopup() {
+    let popupBox = document.querySelector(".marqueePop");
+    let popupInfo = popupBox.querySelector(".popup > a");
+    let closeBtn = popupBox.querySelector(".popup .closeBtn");
+    if(window.innerWidth < 768) {
+        marqueeList.forEach(li => {
+            let popName = li.querySelector("a > .hover > .name").cloneNode(true);
+            let popText = li.querySelector("a > .hover > .text").cloneNode(true);
+            let hashTag = li.querySelector("a > .hover > .hashtag").cloneNode(true);
+            li.addEventListener("click",()=>{
+                body.classList.add("on");
+                marquee01.classList.add("on");
+                marquee02.classList.add("on");
+                popupBox.style.display = "block";
+                popupInfo.replaceChildren();
+                popupInfo.appendChild(popName);
+                popupInfo.appendChild(popText);
+                popupInfo.appendChild(hashTag);
+            })
+        });
+    } else {
+        marqueeList.forEach(li => {
+            li.addEventListener("click",()=>{
+                body.classList.remove("on");
+                marquee01.classList.remove("on");
+                marquee02.classList.remove("on");
+                popupBox.style.display = "none";
+            })
+        });
+    }
+
+    closeBtn.addEventListener("click", ()=>{
+        body.classList.remove("on");
+        marquee01.classList.remove("on");
+        marquee02.classList.remove("on");
+        popupBox.style.display = "none";
+    });
+};
+
+let resizetime;
+
+window.addEventListener("resize",()=>{
+    clearTimeout(resizetime);
+    resizetime = setTimeout(()=>{
+        marqueeHover();
+        mobilePopup();
+    },300);
+})
 
 // alumni 섹션 fade effect
 let alumni = document.querySelector(".alumni");
